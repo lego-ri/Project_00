993d5b48ba41267bb756cba62b46c2b211e67ddb
lego_ri <lego.ri@outlook.cz>
2024-08-09 09:08:39 +0200

    pridana cast my_new_feature do kodu

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+# zakladni Hello World funkce
+print("Hello World!")
+
+# Volani jine funkce zobrazujici moje jmeno
+import myName
+
+name = myName.get_name()
+print("Hello {}".format(name))
+
+# zmena my_new_feature pro git
+print("My New Feature")
+
